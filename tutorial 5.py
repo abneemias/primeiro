0ca32ce360cd192ebd3bcae69cc4336fea3b9614
Neemias Abner <neemiasabner12@hotmail.com>
2021-02-16 20:47:21 -0300

    Comando OSC

--- tutorial 5.py
+++ tutorial 5.py
@@ -1,8 +1,42 @@
 from flask import Flask, redirect, url_for, render_template, request, session
 from datetime import timedelta
 import easygui
+from pythonosc.udp_client import SimpleUDPClient
+import time
+
+ip = "192.168.1.111"
+port = 8089
+
+client = SimpleUDPClient(ip, port)  # Create client
+
+def certa():
+    client.send_message("/1/push1", 1.0)
+    time.sleep(0.5)
+    client.send_message("/1/push1", 0.0)
+    redefinir()
+    pass
 
 
+def errada():
+    client.send_message("/1/push2", 1.0)
+    time.sleep(0.5)
+    client.send_message("/1/push2", 0.0)
+    redefinir()
+    pass
+
+
+def amarelo():
+    client.send_message("/1/push3", 1.0)
+    time.sleep(0.5)
+    client.send_message("/1/push3", 0.0)
+    pass
+
+def azul():
+    client.send_message("/1/push4", 1.0)
+    time.sleep(0.5)
+    client.send_message("/1/push4", 0.0)
+    pass
+    
 app = Flask(__name__)
 app.secret_key = "hello"
 app.permanent_session_lifetime = timedelta(minutes=5)
@@ -15,8 +49,16 @@ def definir(nome):
     quem=nome
     global primeiro
     primeiro = True
-    easygui.msgbox(""+nome+"!", title="O PRIMEIRO")
-    redefinir()
+    if(nome=="amarelo"):
+        amarelo()
+    else:
+        if(nome=="azul"):
+            azul()
+        else:
+            easygui.msgbox(""+nome+"!", title="O PRIMEIRO")
+            redefinir()
+    
+    pass
     
 def redefinir():
     global primeiro
@@ -47,11 +89,24 @@ def user():
         if request.method == "POST":
             if(not primeiro):
                 definir(user)
-                print(quem)
         return render_template('user.html')
     else:
         return redirect(url_for("login"))
-
+        
+@app.route("/admin", methods=["POST", "GET"])
+def admin():
+    if request.method == 'POST':
+        if request.form['submit_button'] == 'CERTA':
+            certa()
+            return render_template("admin.html")
+        elif request.form['submit_button'] == 'ERRADA':
+            errada()
+            return render_template("admin.html")
+        else:
+            pass # unknown
+    elif request.method == 'GET':
+        return render_template("admin.html") 
+        
 @app.route("/output")
 def saida():
         return "neemias"
